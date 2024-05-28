ec42b752643d71c67221b6c6d3fb27696e60031a
Chris Soliman <chris201020112@gmail.com>
2024-05-28 16:00:19 +0300

    google authentication

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -1,12 +1,40 @@
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb.ts";
 
 export default NextAuth({
   providers: [
-    // OAuth authentication providers...
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: "select_account",  // Ensure users are prompted to select an account
+        },
+      },
     }),
-  ]
-})
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ session, token }) {
+      console.log("Session callback:", session);
+      return session;
+    },
+    async jwt({ token, user }) {
+      console.log("JWT callback:", token);
+      return token;
+    },
+  },
+  events: {
+    async signIn(message) { console.log("User signed in:", message); },
+    async signOut(message) { console.log("User signed out:", message); },
+    async createUser(message) { console.log("User created:", message); },
+    async updateUser(message) { console.log("User updated:", message); },
+    async linkAccount(message) { console.log("Account linked:", message); },
+    async session(message) { console.log("Session event:", message); }
+  }
+});
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,17 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
+
+  const { data: session } = useSession()
+  if(session) {
+    console.log(session)
+    return <>
+      Signed in as {session.user.email} <br/>
+      <button onClick={() => signOut()}>Sign out</button>
+    </>
+  }
+
+
   return (
     <>
       <div className="flex flex-col min-h-screen justify-center items-center max-w-4xl m-auto">
@@ -8,9 +21,11 @@ export default function Home() {
         <p className=" font-medium my-4 ">
           An account is needed to view this page
         </p>
+        {session}
 
         <button
           className="inline-block rounded border border-indigo-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+          onClick={() => signIn('google')}
         >
           Sign In with Google
         </button>
