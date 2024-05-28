642a5331b69adef17fbd9337c0d8b76dd01ced13
Chris Soliman <chris201020112@gmail.com>
2024-05-28 17:18:33 +0300

    Dashboard and header

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,15 @@
 import "@/styles/globals.css";
 import { Poppins } from "next/font/google";
 import { SessionProvider } from "next-auth/react"
+import Header from "@/components/Header";
 
 const inter = Poppins({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <main className={`${inter.className}`}>
+    <main className={`p-4 ${inter.className}`}>
       <SessionProvider session={session}>
+        <Header />
       <Component {...pageProps} />
       </SessionProvider>
     </main>
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,16 +1,82 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
+import Link from "next/link";
 
 export default function Home() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        <header>
+          <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+            <div className="sm:flex sm:items-center sm:justify-between">
+              <div className="text-center sm:text-left">
+                <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+                  Welcome Back,{" "}
+                  <span class="text-green-700 ">{session.user.name}!</span>
+                </h1>
 
-  const { data: session } = useSession()
-  if(session) {
-    console.log(session)
-    return <>
-      Signed in as {session.user.email} <br/>
-      <button onClick={() => signOut()}>Sign out</button>
-    </>
+                <p className="mt-1.5 text-md text-gray-500 max-w-lg">
+                  View statistics of your business, manage and add products. 🎉
+                </p>
+              </div>
+
+              <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
+                <Link
+                  className="inline-flex items-center justify-center gap-1.5 rounded-lg border border-gray-200 px-5 py-3 text-gray-500 transition hover:bg-gray-50 hover:text-gray-700 focus:outline-none focus:ring"
+                  href={'/products'}
+                >
+                  <span className="text-md font-medium"> View Products </span>
+
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+                    />
+                  </svg>
+                </Link>
+
+                <button
+                  className="inline-flex items-center justify-center gap-1.5 rounded-lg border border-greem-200 px-5 py-3 text-green-500 transition hover:bg-green-50 hover:text-green-700 focus:outline-none focus:ring"
+                  type="button"
+                >
+                  <span className="text-md font-medium"> View Website </span>
+
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </header>
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8">
+          <div className="h-32 rounded-lg bg-gray-200"></div>
+          <div className="h-32 rounded-lg bg-gray-200"></div>
+          <div className="h-32 rounded-lg bg-gray-200"></div>
+          <div className="h-32 rounded-lg bg-gray-200"></div>
+        </div>
+      </>
+    );
   }
-
 
   return (
     <>
@@ -24,12 +90,11 @@ export default function Home() {
         {session}
 
         <button
-          className="inline-block rounded border border-indigo-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
-          onClick={() => signIn('google')}
+          className="inline-block rounded border border-indigo-600 bg-blue-600 px-12 py-3 text-md font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+          onClick={() => signIn("google")}
         >
           Sign In with Google
         </button>
-
       </div>
     </>
   );
