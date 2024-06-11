38a38942ffad1d79bee8366c01973092709ca445
Chris Soliman <chris201020112@gmail.com>
2024-06-11 18:30:45 +0300

    Schema & Mongodb connection

--- app-frontend/pages/_app.js
+++ app-frontend/pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
 
+import { Poppins } from "next/font/google";
+
+const inter = Poppins({ subsets: ["latin"], weight: '400' });
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <main className={`${inter.className} min-h-screen p-4 bg-background`}>
+        <Component {...pageProps} />;
+      </main>
+    </>
+  );
 }
