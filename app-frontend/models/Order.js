a28bf598db663c315d21237b8fdb82525fbe4578
Chris Soliman <chris201020112@gmail.com>
2024-06-23 14:14:48 +0300

    order model

---
+++ app-frontend/models/Order.js
@@ -0,0 +1,14 @@
+import { Schema, model, models } from "mongoose";
+
+const OrderSchema = new Schema({
+    line_items: Object,
+    name: String,
+    email: String,
+    city: String,
+    zip: String,
+    state: String,
+    address: String,
+    paid: Boolean,
+}, { timestamps: true})
+
+export const Order = models?.Order || model('Order', OrderSchema)
