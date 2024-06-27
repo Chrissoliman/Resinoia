951bf99848216aef253e6b99026aaa7df3d4712d
Chris Soliman <chris201020112@gmail.com>
2024-06-27 14:51:42 +0300

    small changes

--- app-frontend/components/Hero.js
+++ app-frontend/components/Hero.js
@@ -76,14 +76,14 @@ export default function Hero({ product }) {
                 <div className="hidden lg:block absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
                   <div className="flex items-center space-x-6 lg:space-x-8">
                     <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                      <div className="w-64 h-72 overflow-hidden rounded-lg border border-secondary transform rotate-3 translate-x-4 hover:-rotate-6 hover:translate-x-8 transition-transform duration-300 ease-in-out">
+                      <div className="w-64 h-72 overflow-hidden rounded-lg border border-secondary transform -rotate-4 translate-x-2 hover:rotate-8 hover:translate-x-3 transition-transform duration-300 ease-in-out">
                         <img
                           src={product.images[0]}
                           alt=""
                           className="h-full w-full object-cover object-center"
                         />
                       </div>
-                      <div className="w-64 h-72 overflow-hidden rounded-lg border border-secondary transform -rotate-2 translate-x-2 hover:rotate-4 hover:translate-x-4 transition-transform duration-300 ease-in-out">
+                      <div className="w-64 h-72 overflow-hidden rounded-lg border border-secondary transform -rotate-4 translate-x-2 hover:rotate-8 hover:translate-x-3 transition-transform duration-300 ease-in-out">
                         <img
                           src={product.images[1]}
                           alt=""
@@ -92,7 +92,7 @@ export default function Hero({ product }) {
                       </div>
                     </div>
                     <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                      <div className="w-64 h-72 overflow-hidden rounded-lg border border-secondary transform rotate-1 translate-x-3 hover:-rotate-2 hover:translate-x-4 transition-transform duration-300 ease-in-out">
+                      <div className="w-64 h-72 overflow-hidden rounded-lg border border-secondary transform -rotate-4 translate-x-2 hover:rotate-8 hover:translate-x-3 transition-transform duration-300 ease-in-out">
                         <img
                           src={product.images[2]}
                           alt=""
