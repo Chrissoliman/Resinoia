b45f0a7858eaffd128e872cd07ab2661a19cdb52
Chris Soliman <chris201020112@gmail.com>
2024-06-27 15:19:31 +0300

    changing price of product schema

--- frontend/models/Product.js
+++ frontend/models/Product.js
@@ -3,10 +3,11 @@ const { Schema, models, model } = require("mongoose");
 const ProductSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
-    price: {type: String, required: true},
+    price: [{type: String, required: true}],
     images: [{type: String}],
     category: {type: String, required: true},
     heroProduct: {type: String, required: true},
+    sizes: [{type: String}]
 })
 
 export const Product = models.Product || model('Product', ProductSchema)
