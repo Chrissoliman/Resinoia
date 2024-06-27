d51bb4b5e29d78c15da0d8d42388541d5055c92e
Chris Soliman <chris201020112@gmail.com>
2024-06-27 17:11:11 +0300

    product schema fix

--- frontend/models/Product.js
+++ frontend/models/Product.js
@@ -3,11 +3,10 @@ const { Schema, models, model } = require("mongoose");
 const ProductSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
-    price: [{type: String, required: true}],
+    price: [{type: String}],
     images: [{type: String}],
     category: {type: String, required: true},
-    heroProduct: {type: String, required: true},
-    sizes: [{type: String}]
+    heroProduct: {type: String, required: true}
 })
 
 export const Product = models.Product || model('Product', ProductSchema)
