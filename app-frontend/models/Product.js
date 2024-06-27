bc5e8e29db9d78571b189d680d6ba4e1b96200ee
Chris Soliman <chris201020112@gmail.com>
2024-06-27 17:15:17 +0300

    product schema in website

--- app-frontend/models/Product.js
+++ app-frontend/models/Product.js
@@ -3,8 +3,10 @@ const { Schema, models, model } = require("mongoose");
 const ProductSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
-    price: {type: String, required: true},
-    images: [{type: String}]
+    price: [{type: String}],
+    images: [{type: String}],
+    category: {type: String, required: true},
+    heroProduct: {type: String, required: true}
 })
 
 export const Product = models.Product || model('Product', ProductSchema)
