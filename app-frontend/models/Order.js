594e4c1ba8f35a8952d5075aa778178a6e09fef5
Chris Soliman <chris201020112@gmail.com>
2024-07-01 17:57:24 +0300

    order schema

--- app-frontend/models/Order.js
+++ app-frontend/models/Order.js
@@ -9,10 +9,7 @@ const OrderSchema = new Schema({
     state: String,
     address: String,
     phone: String,
-    letter: String,
-    size: String,
-    notes: String,
-    paid: Boolean,
+    status: Boolean,
 }, { timestamps: true})
 
 export const Order = models?.Order || model('Order', OrderSchema)
