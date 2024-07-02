1f7922e5110dd08731212d5cd670b883697b95ca
Chris Soliman <chris201020112@gmail.com>
2024-07-02 19:59:53 +0300

    query to find hero

--- app-frontend/pages/index.js
+++ app-frontend/pages/index.js
@@ -6,87 +6,7 @@ import mongooseConnect from "@/lib/mongoose";
 import { Product } from "@/models/Product";
 
 export default function Home({featureProduct, newProducts, collectionProduct}) {
-  // featureProduct, newProducts, collectionProduct
-  const products = [{
 
-      id: 1,
-      title: "Beach Coasters",
-      description:
-        "Sed pretium pellentesque eros, sit amet ultricies arcu malesuada in. Morbi nec venenatis lectus. Proin ultricies sed magna nec condimentum. Maecenas eu mauris et ante viverra ornare. Nunc sagittis erat eget dui rutrum, vel tempus sem mollis. Aenean hendrerit vitae lorem nec maximus. Aliquam vitae justo nisl. Nulla ex nisl, tincidunt in leo non, dapibus pretium libero. Quisque tincidunt sollicitudin suscipit.",
-      images: [
-        "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207417/Resinoia/file_1718207409747.jpg",
-        "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-        "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-        "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-      ],
-      price: 1234,
-  },{
-
-    id: 1,
-    title: "Beach Coasters",
-    description:
-      "Sed pretium pellentesque eros, sit amet ultricies arcu malesuada in. Morbi nec venenatis lectus. Proin ultricies sed magna nec condimentum. Maecenas eu mauris et ante viverra ornare. Nunc sagittis erat eget dui rutrum, vel tempus sem mollis. Aenean hendrerit vitae lorem nec maximus. Aliquam vitae justo nisl. Nulla ex nisl, tincidunt in leo non, dapibus pretium libero. Quisque tincidunt sollicitudin suscipit.",
-    images: [
-      "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207417/Resinoia/file_1718207409747.jpg",
-      "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-      "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-      "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-    ],
-    price: 1234,
-},{
-
-  id: 1,
-  title: "Beach Coasters",
-  description:
-    "Sed pretium pellentesque eros, sit amet ultricies arcu malesuada in. Morbi nec venenatis lectus. Proin ultricies sed magna nec condimentum. Maecenas eu mauris et ante viverra ornare. Nunc sagittis erat eget dui rutrum, vel tempus sem mollis. Aenean hendrerit vitae lorem nec maximus. Aliquam vitae justo nisl. Nulla ex nisl, tincidunt in leo non, dapibus pretium libero. Quisque tincidunt sollicitudin suscipit.",
-  images: [
-    "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207417/Resinoia/file_1718207409747.jpg",
-    "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-    "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-    "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-  ],
-  price: 1234,
-},{
-
-  id: 1,
-  title: "Beach Coasters",
-  description:
-    "Sed pretium pellentesque eros, sit amet ultricies arcu malesuada in. Morbi nec venenatis lectus. Proin ultricies sed magna nec condimentum. Maecenas eu mauris et ante viverra ornare. Nunc sagittis erat eget dui rutrum, vel tempus sem mollis. Aenean hendrerit vitae lorem nec maximus. Aliquam vitae justo nisl. Nulla ex nisl, tincidunt in leo non, dapibus pretium libero. Quisque tincidunt sollicitudin suscipit.",
-  images: [
-    "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207417/Resinoia/file_1718207409747.jpg",
-    "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-    "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-    "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-  ],
-  price: 1234,
-},{
-
-  id: 1,
-  title: "Beach Coasters",
-  description:
-    "Sed pretium pellentesque eros, sit amet ultricies arcu malesuada in. Morbi nec venenatis lectus. Proin ultricies sed magna nec condimentum. Maecenas eu mauris et ante viverra ornare. Nunc sagittis erat eget dui rutrum, vel tempus sem mollis. Aenean hendrerit vitae lorem nec maximus. Aliquam vitae justo nisl. Nulla ex nisl, tincidunt in leo non, dapibus pretium libero. Quisque tincidunt sollicitudin suscipit.",
-  images: [
-    "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207417/Resinoia/file_1718207409747.jpg",
-    "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-    "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-    "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-  ],
-  price: 1234,
-},];
-  const product = {
-
-      id: 1,
-      title: "Beach Coasters",
-      description:
-        "Sed pretium pellentesque eros, sit amet ultricies arcu malesuada in. Morbi nec venenatis lectus. Proin ultricies sed magna nec condimentum. Maecenas eu mauris et ante viverra ornare. Nunc sagittis erat eget dui rutrum, vel tempus sem mollis. Aenean hendrerit vitae lorem nec maximus. Aliquam vitae justo nisl. Nulla ex nisl, tincidunt in leo non, dapibus pretium libero. Quisque tincidunt sollicitudin suscipit.",
-      images: [
-        "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207417/Resinoia/file_1718207409747.jpg",
-        "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-        "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-        "https://res.cloudinary.com/dxmmprgaa/image/upload/v1718207414/Resinoia/file_1718207409721.jpg",
-      ],
-      price: 1234,
-  };
   return (
     <>
       <Hero product={featureProduct} />
@@ -107,14 +27,8 @@ export default function Home({featureProduct, newProducts, collectionProduct}) {
 export async function getServerSideProps() {
   await mongooseConnect();
 
-  const featureId = "6681918c01f4fec0c35da695";
-  const collectionId = "668191c601f4fec0c35da69d";
-
-  // const featureId = "6669c3c6363f7b9b3a09b571";
-  // const collectionId = "6669c3c6363f7b9b3a09b571";
-
-  const featureProduct = await Product.findById(featureId);
-  const collectionProduct = await Product.findById(collectionId);
+  const featureProduct = await Product.findOne({heroProduct: 'yes'});
+  const collectionProduct = await Product.findOne({collectionProduct: 'yes'});
   const newProducts = await Product.find({}, null, {sort: {'_id': 1}, limit: 5})
 
   return {
