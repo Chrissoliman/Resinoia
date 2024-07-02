9c1c7a96b18d036f6175ebbfed567a316f242ef4
Chris Soliman <chris201020112@gmail.com>
2024-07-02 20:30:54 +0300

    total Income

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -24,6 +24,12 @@ export default function Home() {
     });
   }, []);
 
+  let totalIncome = 0
+
+  for (const {totalPrice} of orders) {
+    totalIncome += totalPrice
+  }
+
   
   if (session) {
     return (
@@ -112,23 +118,6 @@ export default function Home() {
 
                 <div class="text-3xl text-black">{orders.length}</div>
 
-                <div class="flex items-center space-x-1 rtl:space-x-reverse text-sm font-medium text-red-600">
-                  <span>3% decrease</span>
-
-                  <svg
-                    class="w-4 h-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M12 13a1 1 0 100 2h5a1 1 0 001-1V9a1 1 0 10-2 0v2.586l-4.293-4.293a1 1 0 00-1.414 0L8 9.586 3.707 5.293a1 1 0 00-1.414 1.414l5 5a1 1 0 001.414 0L11 9.414 14.586 13H12z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </div>
               </div>
             </div>
 
@@ -138,25 +127,8 @@ export default function Home() {
                   <span>Total Income</span>
                 </div>
 
-                <div class="text-3xl text-black">3543</div>
+                <div class="text-3xl text-black">{totalIncome}</div>
 
-                <div class="flex items-center space-x-1 rtl:space-x-reverse text-sm font-medium text-green-600">
-                  <span>7% increase</span>
-
-                  <svg
-                    class="w-4 h-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                </div>
               </div>
             </div>
           </div>
